f615a16cf2ca2352cd67de08e384c8f893293a7d
jiuyun <jiuyunhziee@163.com>
2023-08-07 14:46:32 +0800

    更新iwdt和reliability的demo注释或者说明

--- components/components/demo/src/iwdt_demo.c
+++ components/components/demo/src/iwdt_demo.c
@@ -19,7 +19,7 @@
 /* Private macro-----------------------------------------------------------*/
 /* Private variablesr------------------------------------------------------*/
 
-/** \brief iwdt normal mode
+/** \brief IWDT非中断demo，溢出时间1s，不开启报警中断
  * 
  *  \param[in] none
  *  \return error code
@@ -30,14 +30,11 @@ int iwdt_normal_demo(void)
 
 	csi_iwdt_init(IWDT_TO_1024);			//初始化看门狗，溢出时间为1000ms(系统复位时间)
 	csi_iwdt_open();						//打开看门狗
-	mdelay(500);							//延时500ms
-	//mdelay(520);
 	csi_iwdt_feed();						//喂狗
-	mdelay(500);
 			
 	return iRet;
 }
-/** \brief iwdt working with interrupt
+/** \brief IWDT中断demo，溢出时间1s，报警中断时间0.5s
  * 
  *  \param[in] none
  *  \return error code
@@ -47,9 +44,8 @@ int iwdt_irq_demo(void)
 	int iRet = 0;
 
 	csi_iwdt_init(IWDT_TO_1024);					//初始化看门狗，溢出时间为1000ms(系统复位时间)
-	csi_iwdt_irq_enable(IWDT_ALARMTO_2_8, ENABLE);	//使能看门狗报警中断，报警时间为4/8溢出时间
+	csi_iwdt_irq_enable(IWDT_ALARMTO_4_8, ENABLE);	//使能看门狗报警中断，报警时间为4/8溢出时间
 	csi_iwdt_open();								//打开看门狗
-	//mdelay(700);
 
 	return iRet;
 }
